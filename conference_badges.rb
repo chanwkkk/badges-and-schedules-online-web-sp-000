24fd5744743b92f6e25ba0898df672555f8d32c5
Yingqi Chen <chanwk18@hotmail.com>
2019-03-22 21:55:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,13 @@ def badge_maker(name)
   return  "Hello, my name is #{name}."
 end
 
-
+require 'pry'
 def batch_badge_creator(array)
   array2=[]
   array.each do|name|
     array2<<badge_maker(name)
   end
+  binding.pry
 return array2
 
 end
