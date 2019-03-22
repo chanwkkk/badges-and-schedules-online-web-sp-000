24eadba32548cdf78dbdb23908450e1df8c3fb85
Yingqi Chen <chanwk18@hotmail.com>
2019-03-22 22:22:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,11 @@ end
 
 
 def batch_badge_creator(array)
-  array.collect {|name| badge_maker(name)}
+  array.collect {|name| badge_maker(name)}#collect saves a lot of work because it returns the new array directly!
     #array2.push(message)
 end
+
+def assign_rooms(array)
+  array.each_with_index.collect do |name,index|
+    return 
+    
